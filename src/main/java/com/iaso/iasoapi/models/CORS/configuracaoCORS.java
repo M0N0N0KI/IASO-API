208c84f7b48b9823579023483427088b809573f4
KevinVinagre <kevin.cv.br@gmail.com>
2021-05-27 23:46:32 -0400

    ativação

---
+++ src/main/java/com/iaso/iasoapi/models/CORS/configuracaoCORS.java
@@ -0,0 +1,16 @@
+package com.iaso.iasoapi.models.CORS;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class configuracaoCORS implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
